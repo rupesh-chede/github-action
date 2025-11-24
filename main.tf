d5694f020c97037ee2b8a34438e584a10882b4e4
Rupesh <rupeshchede60@gmail.com>
2025-11-24 16:25:37 +0530

    github action

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "Dev-vpc"
+    }
+}
+
+resource "aws_subnet" "sub1" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.name.id
+    tags = {
+      Name = "Sub-1"
+    }
+}
+
+resource "aws_subnet" "sub2" {
+    cidr_block = "10.0.2.0/24"
+    vpc_id = aws_vpc.name.id
+    tags = {
+      Name = "Sub-2"
+    }
+
+}
